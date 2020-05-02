18d83092a585163b0797ef7e7072948cd3105961
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-05-01 19:07:37 -0600

    Replace Point_2df with Point<float>

--- OP2-Landlord/Control.h
+++ OP2-Landlord/Control.h
@@ -32,7 +32,7 @@ public:
 	void font(Font& font);
 
 	void position(const Point_2d& pos);
-	void position(const Point_2df& pos);
+	void position(const Point<float>& pos);
 	void position(float x, float y);
 
 	float positionX();
