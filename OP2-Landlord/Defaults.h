974d62fc3f367f125c256dbe402624da6696db2a
Leeor Dicker <leeor_net@yahoo.com>
2023-06-21 20:59:47 -0400

    Removing all source/project files to begin the project fresh

--- OP2-Landlord/Defaults.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-const std::string	UI_TEXTFIELD_DEFAULT_WIDTH			= "50";
-const std::string	UI_TEXTFIELD_DEFAULT_HEIGHT			= "50";
-
-const std::string	EDITOR_MAPS_PATH					= "maps/";
-const std::string	EDITOR_NEW_MAP_NAME					= "New Map";
--- OP2-Landlord/Fonts.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <NAS2D/Resource/Font.h>
-#include <NAS2D/Resource/ResourceCache.h>
-
-
-inline NAS2D::ResourceCache<NAS2D::Font, std::string, unsigned int> fontCache;
